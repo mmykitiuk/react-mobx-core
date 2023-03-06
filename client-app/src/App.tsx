9e76e5dc895ebc5ff57ff19cb0cecfbf01cac927
Darrell Mykitiuk (Mick) <dmykitiuk@epcor.com>
2023-03-05 21:08:28 -0700

    End of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 
 function App() {
+  const [activities,setActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        console.log(response);
+        setActivities(response.data)
+      })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Please edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header as='h2' icon='users' content='Reactivities'/>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <List>
+          {activities.map((activity:any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+        {/* <Button>Test</Button> */}
+
+
     </div>
   );
 }
